d9961d32a7b408d01707cc4edf3635fd95936a4d
developers-codz <kaj123al2002@gmail.com>
2022-08-27 15:37:52 +0530

    fix:seacrh

--- src/function/getSearchedData.js
+++ src/function/getSearchedData.js
@@ -1,15 +1,18 @@
 export const getSearchFilteredData = (data, text) => {
     if (text === "") return data;
+    const query = text.toLowerCase()
     return data.filter((item) => {
       const smallCasedName = item.name.toLowerCase();
       const smallCasedState = item.location.state.toLowerCase();
       const smallCasedCity = item.location.city.toLowerCase();
       const smallCasedAddress = item.location.address.toLowerCase();
+      const smallCasedPropertyType = item["property-type"].toLowerCase();
       return (
-        smallCasedName.includes(text.toLowerCase()) ||
-        smallCasedState.includes(text.toLowerCase()) || 
-        smallCasedCity.includes(text.toLowerCase())  || 
-        smallCasedAddress.includes(text.toLowerCase())
+        smallCasedName.includes(query) ||
+        smallCasedState.includes(query) || 
+        smallCasedCity.includes(query)  || 
+        smallCasedAddress.includes(query)  ||
+        smallCasedPropertyType.includes(query)
       );
     });
   };
