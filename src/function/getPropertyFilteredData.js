ca1a30b2bbb09a7b770a7d03c9d720dfdd75641c
developers-codz <kaj123al2002@gmail.com>
2022-08-27 13:08:23 +0530

    fix:in functionality

--- src/function/getPropertyFilteredData.js
+++ src/function/getPropertyFilteredData.js
@@ -1,6 +1,4 @@
 export const getPropertyFilteredData = (data,text) =>{
-    console.log(text)
-    console.log(data)
     if(text === "") return data;
     return data.filter(item => item["property-type"] === text)
 }
